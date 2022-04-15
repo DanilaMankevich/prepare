e9309cb214ee4ff42e55ceaae9095c84e5d6f498
DanilaMankevich <91117275+DanilaMankevich@users.noreply.github.com>
2022-04-15 16:03:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
+
 @NgModule({
+  //декоратор, который добавляет новые возможности для класса
   declarations: [
-    AppComponent
+    //есть такой компонент имей в виду
+    AppComponent,
+    CardComponent,
+    FormComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  providers: [], //храним сервисы интерсепторы и тд
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
